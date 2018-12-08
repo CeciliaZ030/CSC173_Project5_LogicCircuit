12c8409ae70067b31d421ffec3040bd40cf02acb
Student <ceciliazhang@cecilias-macbook-pro.local>
2018-12-07 20:35:13 -0500

    Initial Commit

---
+++ Logic_Parser/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Logic_Parser
+//
+//  Created by Cecilia Zhang on 12/7/18.
+//  Copyright © 2018 Cecilia Zhang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
